b0ab76e34edb8001ff9381cdbcab63d63e15b5ab
j-te <jacob.tevendale@googlemail.com>
2025-01-23 14:50:46 +0000

    repro resize error in WebGPU

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,16 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     >
     </babylon-viewer>
 `
+
+const viewer: any = document.querySelector('babylon-viewer');
+if (viewer) {
+    viewer.addEventListener('viewerready', () => {
+        const scene = viewer.viewerDetails.scene;
+        const resizeListener = () => {
+            scene.render();
+        };
+        window.addEventListener("resize", resizeListener, true);
+    });
+} else {
+    console.error("Did not find Viewer!")
+}
