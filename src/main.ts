6d354150fefcd2703f42b80241d0cd49a2a8d748
j-te <jacob.tevendale@googlemail.com>
2025-01-23 14:22:44 +0000

    WebGPU and animation-auto-play attributes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <babylon-viewer
         source="https://raw.githubusercontent.com/BabylonJS/Assets/master/meshes/ufo.glb"
         environment="https://cdn.jsdelivr.net/npm/@babylonjs/viewer@preview/assets/photoStudio.env"
+        animation-auto-play
+        engine="WebGPU"
     >
     </babylon-viewer>
 `
